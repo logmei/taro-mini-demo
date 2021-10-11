9926f4a6fe813ac848a0920c525c7f4982b4040a
logmei <522844663@qq.com>
2021-10-11 16:58:24 +0800

    detailCard增加可编辑配置

--- src/components/DetailCard/index.tsx
+++ src/components/DetailCard/index.tsx
@@ -1,13 +1,40 @@
-import { View, Text } from '@tarojs/components'
-import React, { ReactNode } from 'react'
+import { View, Text, Block, Input } from '@tarojs/components'
+import React, { ReactNode, useState } from 'react'
 import { StatusProps } from './data'
 import Styles from './index.module.less'
 
 export interface Columns{
   label:string;
-  name?:string;
+  name:string;
   callBack?:(param:any)=>void;
   emptyNoShow?:boolean;
+  edit?:boolean;
+  value?:any;
+}
+// 详情行value
+export const DetailColumnsValue = ({show,showMore,renderColumnsNum,showRowNum,hidden,v,data,edit})=>{
+  const [value,setValue] = useState(v.callBack?v.callBack(data):(data[v.name||'']||''))
+
+  const handlerInput = (e)=>{
+    v.value = e.detail.value
+    setValue(e.detail.value)
+  }
+
+  let Dom = <Block></Block>
+
+  if(show){
+    let valueDom = <Text>{value}</Text>
+    valueDom = edit && v.edit ?<Input value={value} onInput={handlerInput}></Input>:valueDom
+    if(showMore && (renderColumnsNum>showRowNum)){
+      Dom = <View className={Styles.item} style={{display:hidden===false?'flex':'none'}} key={v.name}><Text>{v.label}</Text>：{valueDom}</View>
+    } else{
+      Dom = <View className={Styles.item} key={v.name}><Text>{v.label}</Text>：{valueDom}</View>
+    }
+
+
+  }
+  return Dom
+
 }
 
 interface IndexProps{
@@ -20,8 +47,12 @@ interface IndexProps{
   status?:StatusProps| string,
   suffixStatus?:string,
   onClick?:(params?:any) => void,
-  childStyle?:object
+  childStyle?:object,
+  showMore?:boolean,
+  showRowNum?:number,
+  edit?:boolean
 }
+
 
 const Index:React.FC<IndexProps> = ({
   title,
@@ -34,12 +65,21 @@ const Index:React.FC<IndexProps> = ({
   suffixStatus,
   onClick,
   children,
-  childStyle
+  childStyle,
+  showMore=false,
+  edit = false,
+  showRowNum=10
 })=>{
+  const [hidden,setHidden] = useState<boolean>(showMore)
   const clickHandler = ()=>{
     if(onClick) onClick(data)
   }
-
+  const handlerMore = (e)=>{
+    e.stopPropagation();
+    setHidden(!hidden)
+  }
+  let renderColumnsNum = 0
+  console.log('detailCard')
   return (
     <View
       className={Styles.DetailCard}
@@ -56,8 +96,27 @@ const Index:React.FC<IndexProps> = ({
           if(v.emptyNoShow && !showValue){
             show = false
           }
-          return show && <View className={Styles.item} key={v.name}><Text>{v.label}</Text>：<Text>{v.callBack?v.callBack(data):(data[v.name||'']||'')}</Text></View>
-        })}
+          if(show) renderColumnsNum++
+           return <DetailColumnsValue
+             key={v.name}
+             show={show}
+             showMore={showMore}
+             renderColumnsNum={renderColumnsNum}
+             showRowNum={showRowNum}
+             edit={edit}
+             hidden={hidden}
+             v={v}
+             data={data}
+           ></DetailColumnsValue>
+          })}
+          {
+            showMore && <View className={Styles.more} onClick={handlerMore}>
+              <View className={`iconfont icon-xiangxiajiantoux ${hidden===true?'':Styles.selectShowUp} ${Styles.icon}`}></View>
+
+             {showRowNum<renderColumnsNum && <Text>{hidden?'展开更多':'隐藏显示'}</Text>}
+              </View>
+          }
+
       </View>
      <View className={Styles.right}>
       <View className={Styles.suffixStyle}>{suffix}</View>
